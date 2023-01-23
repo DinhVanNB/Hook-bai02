ed740f464e7a6144978728c07e7c2a66af3a012b
DinhVanNB <phuongvan2430@gmail.com>
2023-01-23 10:42:41 +0700

    add BT

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import React, {useState} from 'react';
+
+export const App=() =>{
+
+
+  return(
+    <div className='container align-items-center'>
+      
+    </div>
+  );
+}
